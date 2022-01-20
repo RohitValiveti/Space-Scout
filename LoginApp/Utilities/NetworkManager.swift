dfa77a795395c6b5ce049e188d9ec1c7d826b5d5
Rohit Valiveti <rohit.valiveti@gmail.com>
2022-01-20 14:16:45 -0500

    Add files via upload

---
+++ LoginApp/Utilities/NetworkManager.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkManager.swift
+//  LoginApp
+//
+//  Created by Rohit Valiveti on 1/20/22.
+//
+
+import Foundation
+import Firebase
+import FirebaseAuth
+
+class NetworkManager{
+    
+    
+}
