9fc392d744f493ac666eedb24ab2a0beac89fb78
Rohit Valiveti <rohit.valiveti@gmail.com>
2022-01-20 14:16:29 -0500

    Add files via upload

---
+++ LoginApp/View Controllers/EditPfpViewController.swift
@@ -0,0 +1,21 @@
+//
+//  EditPfpViewController.swift
+//  LoginApp
+//
+//  Created by Rohit Valiveti on 1/20/22.
+//
+
+import UIKit
+
+class EditPfpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Edit Profile"
+
+    }
+    
+
+
+}
